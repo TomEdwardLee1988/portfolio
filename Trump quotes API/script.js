9749bffb4aaafbac28caf0d153364d8030295f80
TomEdwardLee1988 <125554266+TomEdwardLee1988@users.noreply.github.com>
2023-03-23 15:25:21 +0000

    Added in try/catch to Async functions

--- Trump quotes API/script.js
+++ Trump quotes API/script.js
@@ -1,27 +1,38 @@
 const quoteName = document.querySelector("#user_name");
+//This fetches a quote every time you refresh
 
+//Add in try, catch
 const trumpQuote = async () => {
-  const res = await fetch(
-    "https://api.whatdoestrumpthink.com/api/v1/quotes/random"
-  );
-  const data = await res.json();
+  try {
+    const res = await fetch(
+      "https://api.whatdoestrumpthink.com/api/v1/quotes/random"
+    );
+    const data = await res.json();
 
-  const container = document.querySelector(".container");
-  const newDataElement = document.createElement("div");
-  newDataElement.innerText = data.message;
-  container.appendChild(newDataElement);
+    const container = document.querySelector(".container");
+    const newDataElement = document.createElement("div");
+    newDataElement.innerText = data.message;
+    container.appendChild(newDataElement);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 trumpQuote();
 
+//This is to add the name of a person to a quote
 document.querySelector("#submit").addEventListener("click", async (e) => {
-  e.preventDefault();
-  const res = await fetch(
-    `https://api.whatdoestrumpthink.com/api/v1/quotes/personalized?q=${quoteName.value}`
-  );
-  const data = await res.json();
-  const container = document.querySelector(".container");
-  const newDataElement = document.createElement("div");
-  newDataElement.innerText = data.message;
-  container.appendChild(newDataElement);
+  try {
+    e.preventDefault();
+    const res = await fetch(
+      `https://api.whatdoestrumpthink.com/api/v1/quotes/personalized?q=${quoteName.value}`
+    );
+    const data = await res.json();
+    const container = document.querySelector(".container");
+    const newDataElement = document.createElement("div");
+    newDataElement.innerText = data.message;
+    container.appendChild(newDataElement);
+  } catch (e) {
+    console.log(e);
+  }
 });
